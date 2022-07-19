5415679acde5f134384f4efcaf22fabb0750e81c
Daniel Gardner <bowshanks@gmail.com>
2022-07-19 11:33:34 -0600

    update url validation

--- src/validateUrl.js
+++ src/validateUrl.js
@@ -1,10 +1,15 @@
 const ValidationObj = require('./ValidationObj');
 const urlRegex = require('./urlRegex');
+const commonTLDRegex = /(([a-z]{1,63}\.)[-a-z0-9@:%._+~#=]{1,256}\.(com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil)(?:[-a-z0-9@:%_+.~#?&()//=]*)\b)/i;
 
 module.exports = function validateUrl (url) {
   const validationObj = new ValidationObj();
-  if (url && !url.match(urlRegex)) {
-    validationObj.add_error('Must be a valid url, i.e. https://example.com');
+  if (url) {
+    const urlMatch1 = url.match(urlRegex);
+    const urlMatch2 = url.match(commonTLDRegex);
+    if (!urlMatch1 && !urlMatch2) {
+      validationObj.add_error('Must be a valid url, i.e. https://example.com');
+    }
   }
   return validationObj;
 };
