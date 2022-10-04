5cb2bde4f075386fe271410edbd5c91bbfe7b960
ttangcs <ttangcs@gmail.com>
2022-10-04 15:10:42 -0600

    rename user-facing gmb to gbp

--- src/validateGoogleMyBusiness.js
+++ src/validateGoogleMyBusiness.js
@@ -30,10 +30,10 @@ function validateGMBBody (body, postContentType) {
   const maxCharacters = 1500;
   const validationObj = new ValidationObj();
 
-  if (!body || typeof body !== 'string') validationObj.errors.push({ message: 'GMB requires a summary for every post' });
+  if (!body || typeof body !== 'string') validationObj.errors.push({ message: 'GBP requires a summary for every post' });
   if (body.length > maxCharacters) validationObj.errors.push({ message: 'Summary is too long' });
 
-  if (postContentType === 'reel') validationObj.add_warning('GMB does not support Reels. This post will be published as a normal post.');
+  if (postContentType === 'reel') validationObj.add_warning('GBP does not support Reels. This post will be published as a normal post.');
 
   return validationObj;
 }
@@ -47,52 +47,52 @@ function validateGMBDetails (details) {
   if (details.google_my_business.event) {
     const event = details.google_my_business.event;
     if (!event.title) {
-      result.add_error('GMB Event posts must have a title');
+      result.add_error('GBP Event posts must have a title');
     }
     if (!get(event, 'start_date.year') || !get(event, 'start_date.month') || !get(event, 'start_date.day')
       || get(event, 'start_time.hour') === undefined || get(event, 'start_time.minute') === undefined
     ) {
-      result.add_error('GMB Event posts must have a start date & time');
+      result.add_error('GBP Event posts must have a start date & time');
     }
     if (!get(event, 'end_date.year') || !get(event, 'end_date.month') || !get(event, 'end_date.day')
       || get(event, 'end_time.hour') === undefined || get(event, 'end_time.minute') === undefined
     ) {
-      result.add_error('GMB Event posts must have an end date & time');
+      result.add_error('GBP Event posts must have an end date & time');
     }
   } else if (details.google_my_business.call_to_action) {
     const cta = details.google_my_business.call_to_action;
     if (!cta.action) {
-      result.add_error('GMB Call To Action posts require an action');
+      result.add_error('GBP Call To Action posts require an action');
     }
     if (cta.action && !GMB_CALLS_TO_ACTION.includes(cta.action)) {
-      result.add_error(`GMB unsupported Call To Action: ${cta.action}`);
+      result.add_error(`GBP unsupported Call To Action: ${cta.action}`);
     }
     if (GMB_CALLS_TO_ACTION_REQUIRING_URL.includes(cta.action) && !cta.url) {
-      result.add_error('GMB Call To Action posts require a url');
+      result.add_error('GBP Call To Action posts require a url');
     }
   } else if (details.google_my_business.offer) {
     const offer = details.google_my_business.offer;
     if (!offer.coupon_code) {
-      result.add_error('GMB Offer posts require a coupon code');
+      result.add_error('GBP Offer posts require a coupon code');
     }
     if (!offer.url) {
-      result.add_error('GMB Offer posts require a redemption url');
+      result.add_error('GBP Offer posts require a redemption url');
     }
     if (!offer.terms) {
-      result.add_error('GMB Offer posts require terms of offer to be specified');
+      result.add_error('GBP Offer posts require terms of offer to be specified');
     }
     if (!offer.title) {
-      result.add_error('GMB Offer posts must have a title');
+      result.add_error('GBP Offer posts must have a title');
     }
     if (!get(offer, 'start_date.year') || !get(offer, 'start_date.month') || !get(offer, 'start_date.day')
       || get(offer, 'start_time.hour') === undefined || get(offer, 'start_time.minute') === undefined
     ) {
-      result.add_error('GMB Offer posts must have a start date & time');
+      result.add_error('GBP Offer posts must have a start date & time');
     }
     if (!get(offer, 'end_date.year') || !get(offer, 'end_date.month') || !get(offer, 'end_date.day')
       || get(offer, 'end_time.hour') === undefined || get(offer, 'end_time.minute') === undefined
     ) {
-      result.add_error('GMB Offer posts must have an end date & time');
+      result.add_error('GBP Offer posts must have an end date & time');
     }
   }
 
@@ -105,7 +105,7 @@ function validateGMBMedia (media) {
   const response = { all };
 
   if (!media || media.length !== 1) {
-    all.add_error('Must include exactly 1 image to post to Google My Business');
+    all.add_error('Must include exactly 1 image to post to Google Business Profile');
   } else {
     if (media.some(instance => !isObject(instance.metadata))) {
       all.add_error('Media metadata analysis unavailable. Please check back later.');
