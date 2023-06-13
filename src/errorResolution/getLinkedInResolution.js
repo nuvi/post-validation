2b35c2988a5898acc96d49fc57317d260d9e8fff
Daniel Gardner <dgardner@reputation.com>
2023-06-13 11:00:17 -0600

    🟣RBENT-90630 (#20)
    * account for more errors
    
    * test

--- src/errorResolution/getLinkedInResolution.js
+++ src/errorResolution/getLinkedInResolution.js
@@ -1,5 +1,8 @@
 /* eslint-disable max-len */
 module.exports = function getLinkedInResolution (message) {
+  if (message.includes('The token used in the request has been revoked by the user')) {
+    return 'A user revoked access for this app in LinkedIn. You\'ll need to reauthorize your LinkedIn account before you can continue to publish.';
+  }
   if (message.includes('The token used in the request has expired')) {
     return 'Your LinkedIn access token has expired. You\'ll need to reauthorize your LinkedIn account before you can continue to publish.';
   }
