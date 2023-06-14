4f4f6c2e8ca8cf69bc3a884b2a5cd59790a8494d
bloveall <bloveall@reputation.com>
2023-06-14 14:35:55 -0600

    post validation for stories added

--- src/ValidationObj.js
+++ src/ValidationObj.js
@@ -1,3 +1,6 @@
+/*
+ * This is used to store any errors/warnings when validating different media types that will appear on the front end as errors/warnings
+ */
 module.exports = class ValidationObj {
   constructor () {
     this.errors = [];
