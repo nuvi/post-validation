f7820a05d87ff120a7750a0a386db795fac5d69c
bloveall <bloveall@reputation.com>
2024-03-25 09:39:55 -0600

    fixed regex url parsing

--- src/validateUrl.js
+++ src/validateUrl.js
@@ -1,6 +1,6 @@
 const ValidationObj = require('./ValidationObj');
 const urlRegex = require('./urlRegex');
-const commonTLDRegex = /(([a-z]{1,63}\.)[-a-z0-9@:%._+~#=]{1,256}\.(com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil)(?:[-a-z0-9@:%_+.~#?&()//=]*)\b)/i;
+const commonTLDRegex = /(?!.*[.]{2,})(([a-z]{1,63}\.)[-a-z0-9@:%._+~#=]{1,256}\.(com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil)(?:[-a-z0-9@:%_+.~#?&()//=]*)\b)/i;
 
 module.exports = function validateUrl (url) {
   const validationObj = new ValidationObj();
